434e4407d9c14b16301dbc303025f5fc644d72ef
Azizul Islam <azizul7m@gmail.com>
2023-03-09 17:15:04 +0600

    os config added

--- flake.nix
+++ flake.nix
@@ -2,26 +2,30 @@
   description = "title";
   inputs =
     {
-      nixpkgs = "nixpkgs/nixos-unstable";
+      nixpkgs = {
+        url = "nixpkgs/nixos-unstable";
+        inputs = { };
+      };
     };
   outputs = { self, nixpkgs, nixosConfig, ... }:
     let
       user = "anower";
       host = "nix";
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       lib = nixpkgs.lib;
     in
     {
       nixosConfiguration = {
-        mySystem = inputs.lib.nixosSystem
+        mySystem = lib.nixosSystem
           {
-            system = ${system};
+            system = system;
             modules = [ ./nix/configuration.nix ];
           };
+        nixosConfig = {
+          config = "nixosConfiguration.mySystem";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
       };
     };
 }
