9d1df59813cf3c9f174e373f2a9f8191fddb12c3
Azizul Islam <azizul7m@gmail.com>
2023-03-16 23:51:05 +0600

    host move to module

--- flake.nix
+++ flake.nix
@@ -9,21 +9,17 @@
         inputs.nixpkgs.follows = "nixpkgs";
       };
     };
-  outputs = inputs @{ self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }@ inputs:
     let
       system = "x86_64-linux";
-      # user = "anower";
-      # host = "nix";
+      user = "anower";
+      host = "nix";
       pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       lib = nixpkgs.lib;
     in
     {
-      nixosConfigurations = {
-        ${host} = lib.nixosSystem {
-          inherit system;
-          # nixos host && home-manager
-          modules = [ ./nix ];
-        };
-      };
+      nixosConfigurations = (
+        import ./nix { inherit nixpkgs self inputs host user; }
+      );
     };
 }
