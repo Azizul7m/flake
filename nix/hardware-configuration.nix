25a16abb1997c04b9827fef73b06f3265282676f
Azizul Islam <azizul7m@gmail.com>
2023-03-18 20:16:39 +0600

    hardware config changed

--- nix/hardware-configuration.nix
+++ nix/hardware-configuration.nix
@@ -16,12 +16,12 @@
 
   fileSystems."/" =
     {
-      device = "/dev/disk/by-label/nixos";
+      device = "/dev/disk/by-label/nix";
       fsType = "ext4";
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-label/linuxswap"; }];
+    [{ device = "/dev/disk/by-label/SWAP"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
