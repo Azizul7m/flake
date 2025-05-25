08b8885355d9251e6446d79208740192f37c6a16
Azizul Islam <azizul7m@gmail.com>
2025-05-25 12:06:59 +0600

    conf: docker added

--- host/virtualisation.nix
+++ host/virtualisation.nix
@@ -1,18 +1,8 @@
 { var, ... }: {
   environment.systemPackages = with var.pkgs; [
-    # distrobox
-    # toolbox
-    lxd-lts # Ensure lxd is explicitly listed
-    #vagrant # Lxd like container system
-    # boxbuddy
-    # virt-manager
-    # virt-viewer
-    # spice
-    # spice-gtk
-    # spice-protocol
-    # win-virtio
-    # win-spice
-    # gnome.adwaita-icon-theme
+    distrobox
+    toolbox
+    # lxd-lts # Ensure lxd is explicitly listed
   ];
   virtualisation = {
     # libvirtd = {
@@ -23,31 +13,24 @@
     #     ovmf.packages = [ var.pkgs.OVMFFull.fd ];
     #   };
     # };
-    podman = {
+    docker = {
       enable = true;
-      dockerCompat = true;
-    };
-    lxd = {
-      enable = true;
-      ui.enable = true;
-      zfsSupport = true;
-      recommendedSysctlSettings = true;
-    };
-    lxc = {
-      enable = true;
-      lxcfs.enable = true;
-      systemConfig = ''
-        lxc.lxcpath = /var/lib/lxd/storage-pools
-        lxc.bdev.zfs.root = rpool/safe/lxd
-      '';
+      rootless = {
+        package = var.pkgs.docker;
+        setSocketVariable = true;
+        enable = true;
+      };
+      enableOnBoot = true;
+      autoPrune.enable = true;
+      extraPackages = with var.pkgs; [ criu ];
     };
     spiceUSBRedirection.enable = true;
   };
-  # services = { spice-vdagentd.enable = true; };
+  services = { dockerRegistry.enable = true; };
   # environment.variables = { VAGRANT_DEFAULT_PROVIDER = "libvirt"; };
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.forwarding" = true;
     "net.ipv4.conf.default.forwarding" = true;
   };
-  boot.kernelModules = [ "nf_nat_ftp"  ];
+  boot.kernelModules = [ "nf_nat_ftp" ];
 }
