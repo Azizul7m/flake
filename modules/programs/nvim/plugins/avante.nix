1c6362210dd9acf3e6fd38b676328fdd5c47cc6a
anower <azizul7m@gmail.com>
2025-08-29 22:43:18 +0600

    added: nixvim

--- modules/programs/nvim/plugins/avante.nix
+++ modules/programs/nvim/plugins/avante.nix
@@ -10,12 +10,6 @@
            provider = "dressing";
         };
       };
-      groq = {
-      __inherited_from = "openai";
-      api_key_name = "GROQ_API_KEY";
-      endpoint = "https://api.groq.com/openai/v1/";
-      model = "llama-3.1-70b-versatile";
-    };
     openrouter = {
         __inherited_from = "openai";
         endpoint = "https://openrouter.ai/api/v1";
