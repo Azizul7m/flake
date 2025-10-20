c46d4e391c2bcd8ed787459b42734a20722553e1
anower <azizul7m@gmail.com>
2025-10-20 10:42:10 +0600

    update: encrypt

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,8 @@
 let
-  nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHIbifuLoShpYxc3xPSH4sQL0sSNklv1phF8OFF/RDlf azizul7m@gmail.com";
-in
-{
-  "gemini.age".publicKeys = [ nixos ];
-  "groq.age".publicKeys = [ nixos ];
-  "openai.age".publicKeys = [ nixos ];
+  key =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOnfsvWilvkrwHZ7MfQJSHT2lZfQ7tuVR1G9e0/bAKwy azizul7m@gmail.com";
+in {
+  "gemini.age".publicKeys = [ key ];
+  "syncthing.age".publicKeys = [ key ];
 }
 
