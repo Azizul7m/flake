d9471981f892435eb934fca51631941fe443ba8d
anower <azizul7m@gmail.com>
2025-10-20 09:59:28 +0600

    fish updated

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -1,5 +1,6 @@
-{ pkgs, ... }: with pkgs; {
-  home.packages = ([ ]);
+{ pkgs, ... }:
+with pkgs; {
+  home.packages = [ zoxide atuin starship eza lsd ];
   programs = {
     fish = {
       enable = true;
@@ -8,90 +9,88 @@
         src = pkgs.fishPlugins.grc.src;
       }];
       interactiveShellInit = ''
-        set fish_greeting # Disable greeting
+          set fish_greeting # Disable greeting
 
-        set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+          # navigation
+          alias ..='cd ..'
+          alias ...='cd ../..'
+          alias .3='cd ../../..'
+          alias .4='cd ../../../..'
+          alias .5='cd ../../../../..'
 
-        # navigation
-        alias ..='cd ..'
-        alias ...='cd ../..'
-        alias .3='cd ../../..'
-        alias .4='cd ../../../..'
-        alias .5='cd ../../../../..'
-
-        alias ls='lsd'
-        alias l='lsd -ll'
-        alias la='lsd -a'
-        alias ll='lsd -la'
-        alias cls='clear'
+          alias ls='lsd'
+          alias l='lsd -ll'
+          alias la='lsd -a'
+          alias ll='lsd -la'
+          alias cls='clear'
 
 
-        alias em='emacs -nw'
-        alias emds='emacs --daemon'
-        alias emd='emacsclient -tty'
+          alias em='emacs -nw'
+          alias emds='emacs --daemon'
+          alias emd='emacsclient -tty'
 
-        alias vim='nvim'
-        alias c='code'
-        alias code='code'
-
-
-        alias tas='tmux new-session -A -s default'
-
-        alias df='df -h'
-        alias free='free -m'
-        alias rmi='rm -rf'
-        alias pkill='sudo pkill'
-
-        # get error messages from journalctl
-        alias jctl="journalctl -p 3 -xb"
-
-        alias gap='git add -u'
-        alias gaa='git add .'
-        alias gco='git checkout'
-        alias gc='git clone'
-        alias gp='git push origin'
-        alias gm='git commit -m'
-        alias gb='git branch'
-        alias gs='git status'
-        alias gl='git log'
-        alias gf='git diff'
-
-        alias ranger='joshuto'
-
-        # Play audio files in current dir by type
-        alias playwav='vlc *.wav'
-        alias playogg='vlc *.ogg'
-        alias playmp3='vlc *.mp3'
-
-        # Youtube-Dl
-        alias ytv='yt-dlp -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b"'
-        alias yta='yt-dlp -x --audio-format mp3'
-
-        # Play video files in current dir by type
-        alias playavi='vlc *.avi'
-        alias playmov='vlc *.mov'
-        alias playmp4='vlc *.mp4'
+          alias vim='nvim'
+          alias c='code'
+          alias code='code'
 
 
-        alias tb='nc termbin.com 9999'
-        alias recordterm='asciinema rec'
+          alias tas='tmux new-session -A -s default'
 
-        # Distrobox
-        alias dev='ssh anower@10.155.119.10'
+          alias df='df -h'
+          alias free='free -m'
+          alias rmi='rm -rf'
+          alias pkill='sudo pkill'
+
+          # get error messages from journalctl
+          alias jctl="journalctl -p 3 -xb"
+
+          alias gap='git add -u'
+          alias gaa='git add .'
+          alias gco='git checkout'
+          alias gc='git clone'
+          alias gp='git push origin'
+          alias gm='git commit -m'
+          alias gb='git branch'
+          alias gs='git status'
+          alias gl='git log'
+          alias gf='git diff'
+
+          alias ranger='joshuto'
+
+          # Play audio files in current dir by type
+          alias playwav='vlc *.wav'
+          alias playogg='vlc *.ogg'
+          alias playmp3='vlc *.mp3'
+
+          # Youtube-Dl
+          alias ytv='yt-dlp -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b"'
+          alias yta='yt-dlp -x --audio-format mp3'
+
+          # Play video files in current dir by type
+          alias playavi='vlc *.avi'
+          alias playmov='vlc *.mov'
+          alias playmp4='vlc *.mp4'
 
 
-        alias pdf='sioyek'
-        alias v='alacritty -e nvim'
+          alias tb='nc termbin.com 9999'
+          alias recordterm='asciinema rec'
 
-        alias hotspot='nmcli dev wifi hotspot'
-        alias wpassword='nmcli dev wifi show-password'
+          # Distrobox
+          alias dev='ssh anower@10.155.119.10'
 
-      #  alias hm_rebuild='home-manager switch --flake ~/flake#nixos'
-      #  alias hm_news='home-manager news --flake ~/flake#nixos'
 
-        alias nix_rebuild='sudo nixos-rebuild switch --flake ~/flake#nixos'
-        alias nix_cleanup='sudo nix-collect-garbage -d'
-        alias nix_oldgen='sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old'
+          alias pdf='sioyek'
+          alias v='nvim'
+
+          alias hotspot='nmcli dev wifi hotspot'
+          alias wpassword='nmcli dev wifi show-password'
+
+        #  alias hm_rebuild='home-manager switch --flake ~/flake#nixos'
+        #  alias hm_news='home-manager news --flake ~/flake#nixos'
+
+          alias nix_rebuild='sudo nixos-rebuild switch --flake ~/flake#nixos'
+          alias nix_cleanup='sudo nix-collect-garbage -d'
+          alias nix_oldgen='sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old'
 
       '';
       # Add npm path to PATH
@@ -99,6 +98,7 @@
         set -gx PATH $HOME/.npm_global/bin $HOME/.deno/bin $HOME/.cargo/bin $HOME/.bun/bin $HOME/.avm/bin $HOME/.local/share/solana/install/active_release/bin $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin $HOME/.cargo/bin $HOME/.local/bin $PATH
                  zoxide init --cmd cd fish | source
                  starship init fish | source
+                 atuin init fish | source
       '';
     };
   };
