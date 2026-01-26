9751c7d022ce39fa0efcd455e21ccb1d42230fc1
Azizul Islam <azizul7m@gmail.com>
2026-01-26 13:36:09 +0600

    nvim: srounding

--- src/nvim/lua/plugins/mini.lua
+++ src/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
 	version = "*",
 	config = function()
 		require("mini.pairs").setup()
-		require("mini.surround").setup()
 		require("mini.statusline").setup()
 		require("mini.comment").setup()
 		require("mini.align").setup()
